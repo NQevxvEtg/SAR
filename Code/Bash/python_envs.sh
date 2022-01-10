8e8015f67f7274609b09d44c3a15c6346e5094f3
NQevxvEtg <xguc@protonmail.com>
2022-01-10 05:40:54 +0000

    New commit

--- Code/Bash/python_envs.sh
+++ Code/Bash/python_envs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+wget https://bootstrap.pypa.io/get-pip.py
+
 path="/path/env"
 
 a=(2.7 3.6 3.7 3.8 3.9)
 
 for i in "${a[@]}"
 do
-  wget https://bootstrap.pypa.io/get-pip.py
   sudo /usr/local/bin/python$i get-pip.py
   sudo /usr/local/bin/pip$i install -U pip
   sudo /usr/local/bin/pip$i install -U virtualenv
