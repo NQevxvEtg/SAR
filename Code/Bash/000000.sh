2cfcceccd4cc67ef7adbd1d2b596c1698751851e
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-08-01 14:37:17 -0700

    Update 000000.sh

--- Code/Bash/000000.sh
+++ Code/Bash/000000.sh
@@ -22,5 +22,20 @@ rsync -uaP ~/.[^.]* /dest/
 sudo fuser -v /dev/nvidia*
 sudo kill -9 PID.
 
+# cifs mnt
 mount -t cifs -o username=username,password=password,domain=domain //0.0.0.0/repository /path/repository
+
+# check port
+(echo >/dev/tcp/0.0.0.0/22) &>/dev/null && echo "open" || echo "close"
+
+# mcafee firewall
+/path/McAfee/ens/fw/bin/mfefwcli --fw-rule-add --name 0.0.0.0/24 --action allow --direction either --remote-cidr 0.0.0.0/24
+
+systemctl list-unit-files | grep service_name
+
+ss -ant | grep -E ':80|:443' | wc -l
+
+watch -n 1 "ss -ant | grep -E ':80|:443' | wc -l"
+
+
 
