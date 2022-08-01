553f36350960262a128cdebe984ad5c230e03fda
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-08-01 12:54:51 -0700

    Update pscredential.ps1

--- Code/PowerShell/pscredential.ps1
+++ Code/PowerShell/pscredential.ps1
@@ -1,3 +1,5 @@
+Get-command -module 'Microsoft.PowerShell.SecretManagement'
+
 $params = @{
     Name            = 'vault_name'
     ModuleName      = 'Microsoft.PowerShell.SecretStore'
@@ -12,8 +14,16 @@ Set-Secret -name username -secret (get-credential username)
 
 Set-Secret -name username -secret (get-credential domain\username)
 
+Get-SecretInfo
+
 Set-SecretStoreConfiguration -PasswordTimeout 60
 
 Unlock-SecretStore
 
 Reset-SecretStore
+
+Connect-VIServer -Server fqdn -Protocol https -Credential (Get-Secret username_in_vault)
+
+Get-SecretStoreConfiguration
+
+cd $env:LOCALAPPDATA\Microsoft\PowerShell\secretmanagement\localstore\
