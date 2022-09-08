2aa31116caa6eef386d252a980f0428f7c795745
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-09-08 14:57:56 -0700

    Update 000000.ps1

--- Code/PowerShell/000000.ps1
+++ Code/PowerShell/000000.ps1
@@ -1,5 +1,10 @@
 Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName hostname
 
+# linux tail like
 Get-Content -Path "file.log" -Wait
 
+# remove software
 MsiExec.exe /X{long-uuid-here}
+
+# get info
+wmic /node:hostname product get name,version /format:csv > hostname.csv
