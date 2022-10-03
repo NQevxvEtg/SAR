da681ac08b0bea1088a6dd3e1106c4811038156b
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-10-03 14:44:59 -0700

    Update rhel.sh

--- Code/STIG/BASH/rhel.sh
+++ Code/STIG/BASH/rhel.sh
@@ -1,8 +1,8 @@
 # Red Hat Enterprise Linux 7 Security Technical Implementation Guide :: Version 3, Release: 8 Benchmark Date: 27 Jul 2022
 
 # Vul ID: V-204392
-# for i in `rpm -Va | egrep '^.{1}M|^.{5}U|^.{6}G' | cut -d " " -f 4,5`;do for j in `rpm -qf $i`;do rpm -ql $j --dump | cut -d " " -f 1,5,6,7 | grep $i;done;done
-# send output to output.txt first
+# for i in `rpm -Va | egrep '^.{1}M|^.{5}U|^.{6}G' | cut -d " " -f 4,5`;do for j in `rpm -qf $i`;do rpm -ql $j --dump | cut -d " " -f 1,5,6,7 | grep $i;done;done | tee ouput.txt
+
 while read p; do
 FILENAME=$(awk '{ print $1 }')
 RPMNAME=$(rpm -qf $FILENAME)
