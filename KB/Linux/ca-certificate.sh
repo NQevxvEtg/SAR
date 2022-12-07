bdd0326ce2c095c4da6a2d3bcd20dc2e584b3a0d
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-12-07 11:36:06 -0800

    Update ca-certificate.sh

--- KB/Linux/ca-certificate.sh
+++ KB/Linux/ca-certificate.sh
@@ -1,2 +1,11 @@
 # print ca certs
 awk -v cmd='openssl x509 -noout -subject' '/BEGIN/{close(cmd)};{print | cmd}' < /etc/ssl/certs/ca-certificates.crt
+
+# convert to crt first
+openssl x509 -outform der -in CERTIFICATE.pem -out CERTIFICATE.crt
+
+# copy to ca dir
+cp CERTIFICATE.crt /usr/local/share/ca-certificates
+
+# update ca
+update-ca-certificates
