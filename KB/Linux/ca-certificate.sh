d1c4a1ed359e912a1f048adbb775d8a6578d2d66
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2022-12-08 09:34:16 -0800

    Update ca-certificate.sh

--- KB/Linux/ca-certificate.sh
+++ KB/Linux/ca-certificate.sh
@@ -1,11 +1,13 @@
 # print ca certs
 awk -v cmd='openssl x509 -noout -subject' '/BEGIN/{close(cmd)};{print | cmd}' < /etc/ssl/certs/ca-certificates.crt
 
-# convert to crt first
+# convert to crt first, but this might now work
 openssl x509 -outform der -in CERTIFICATE.pem -out CERTIFICATE.crt
+# alternative is to simply copy pem to crt
+cp CERTIFICATE.pem CERTIFICATE.crt
 
 # copy to ca dir
-cp CERTIFICATE.crt /usr/local/share/ca-certificates
+cp CERTIFICATE.* /usr/local/share/ca-certificates
 
 # update ca
 update-ca-certificates
