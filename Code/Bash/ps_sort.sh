d9a812dfc1287e05bf223b0601571832ced3056f
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2024-08-31 19:36:58 +0000

    New commit

--- Code/Bash/ps_sort.sh
+++ Code/Bash/ps_sort.sh
@@ -15,3 +15,12 @@ NR>1 {printf \
 "%-10s %-6s %-6s %-6s %-11s %-8s %-8s %-8s %-8s %-8s %-20s\n", \
 $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, substr($11, 1, 20)}' | \
 head -n 11
+
+ps -ef --sort=-pid | awk \
+'NR==1 {printf \
+"%-10s %-6s %-6s %-6s %-8s %-8s %-8s %-20s\n", \
+"UID", "PID", "PPID", "C", "STIME", "TTY", "TIME", "CMD"} \
+NR>1 {printf \
+"%-10s %-6s %-6s %-6s %-8s %-8s %-8s %-20s\n", \
+$1, $2, $3, $4, $5, $6, $7, substr($8, 1, 20)}' | \
+head -n 11
