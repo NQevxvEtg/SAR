54df1f9ca8d01388fc8f9b683b20de47f72ac1ca
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2025-06-21 11:01:08 -0400

    Update arch.sh

--- Code/Bash/arch.sh
+++ Code/Bash/arch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script performs the initial setup on the Arch Linux Live ISO environment.
+# It sets up disk encryption, LVM, mounts filesystems, and runs pacstrap.
+# It also generates and executes the second part of the installation script
+# within the chroot environment.
+
 # WARNING: This script contains hardcoded passwords.
 # For production environments, consider more secure methods for password handling.
 
@@ -20,15 +25,98 @@ ARCH_BOOT_PARTITION="/dev/nvme0n1p5"    # The new partition created for Arch's /
 LUKS_PARTITION="/dev/nvme0n1p6"         # The new partition for the Arch LUKS container (rest of space)
 
 
-# --- Part 1: Host System Setup ---
-
 echo "Starting Arch Linux installation script (Part 1: Host System Setup)..."
 
-# Download necessary scripts
-echo "Downloading helper scripts..."
-curl https://raw.githubusercontent.com/NQevxvEtg/SAR/main/Code/Bash/nftable.sh -o nftable.sh
+# Download nftables.sh
+echo "Downloading nftables.sh..."
+curl -o nftable.sh https://raw.githubusercontent.com/NQevxvEtg/SAR/main/Code/Bash/nftable.sh
+chmod +x nftable.sh
 
-chmod +x *.sh
+# Create the second part of the installation script locally
+# This content will be copied to /mnt and executed in chroot
+cat << 'EOF_CHROOT_SCRIPT' > install_part2_chroot.sh
+#!/bin/bash
+
+# This script is executed inside the chroot environment (/mnt).
+# It handles post-pacstrap configuration, package installation,
+# user setup, GRUB, swap, and service enabling.
+
+# Variables are passed as arguments from the host script.
+DEV_PASS="$1"
+ROOT_PASS="$2"
+USER_NAME="$3"
+USER_PASS="$4"
+TIME_ZONE="$5"
+EXISTING_EFI_PARTITION_IN_CHROOT="$6"
+LUKS_PARTITION_IN_CHROOT="$7"
+
+echo "Starting Arch Linux installation script (Part 2: Chroot System Setup)..."
+
+# Install main packages
+echo "Installing core packages for Wayland, GNOME, and Docker..."
+pacman -Syu --noconfirm linux linux-headers linux-lts linux-lts-headers base-devel linux-firmware iwd networkmanager nftables net-tools terminator firefox git go keepassxc grub efibootmgr dosfstools os-prober mtools man rsync bash-completion zsh zsh-completions dnsutils gnome reflector tk code amd-ucode nvidia nvidia-lts nvidia-utils xorg-server xorg-apps xorg-xinit xf86-video-amdgpu mesa xorg-xwayland xfsprogs docker
+
+# Kernel mkinitcpio configuration
+echo "Updating mkinitcpio configuration..."
+sed -i "s/HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/g" /etc/mkinitcpio.conf
+mkinitcpio -P
+
+# Locale setup
+echo "Setting up locale..."
+sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+locale-gen
+
+# User and password setup
+echo "Setting root password..."
+echo "root:$ROOT_PASS" | chpasswd
+echo "Creating user '$USER_NAME' and setting password..."
+useradd -m -g users -G wheel "$USER_NAME"
+echo "$USER_NAME:$USER_PASS" | chpasswd
+
+# Add user to docker group
+echo "Adding user '$USER_NAME' to the 'docker' group..."
+gpasswd -a "$USER_NAME" docker
+
+# Grub installation and configuration
+echo "Installing Grub..."
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck --efi-directory=/boot/EFI
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+    
+# Configure GRUB for encrypted LVM and Wayland (NVIDIA KMS)
+echo "Updating GRUB configuration for encrypted LVM and Wayland (NVIDIA KMS if applicable)..."
+sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=${LUKS_PARTITION_IN_CHROOT}:lvm:allow-discards loglevel=3 quiet nvidia_modeset=1\"/g" /etc/default/grub
+sed -i "s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=\"1>2\"/g" /etc/default/grub
+    
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Swap setup (Swap File on XFS)
+echo "Setting up 100GB swap file on XFS filesystem..."
+fallocate -l 100G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+# Add to fstab
+echo '/swapfile none swap defaults 0 0' | tee -a /etc/fstab
+swapon /swapfile
+
+# Timezone setup
+echo "Setting timezone to $TIME_ZONE..."
+timedatectl set-timezone "$TIME_ZONE"
+
+# Enable services
+echo "Enabling systemd services..."
+systemctl enable NetworkManager
+systemctl enable systemd-timesyncd
+systemctl enable gdm
+systemctl enable docker
+
+# Clean up the chroot script itself
+echo "Cleaning up installer script from chroot environment..."
+rm /install_part2_chroot.sh
+
+echo "Arch Linux installation (Part 2) complete. You can now reboot and log in."
+EOF_CHROOT_SCRIPT
+
+chmod +x install_part2_chroot.sh
 
 # --- Automated disk partitioning (fdisk) has been REMOVED. ---
 # This is now a MANUAL step you must perform before running this script
@@ -76,107 +164,28 @@ genfstab -U -p /mnt >> /mnt/etc/fstab
 
 # Initial pacstrap
 echo "Running pacstrap..."
-# Removed btrfs-progs, added xfsprogs for XFS
-pacstrap /mnt base vim xfsprogs
+pacstrap /mnt base vim xfsprogs # xfsprogs for XFS support inside chroot
 
-# Copy this script and other necessary files to the target system
+# Copy nftables script and the generated chroot script to the target system
 echo "Copying scripts and files to /mnt..."
 cp nftable.sh /mnt/nftable.sh
-cp "$0" /mnt/arch-installer-chroot.sh
+cp install_part2_chroot.sh /mnt/install_part2_chroot.sh
 
-# Make the copied script executable within the chroot
-chmod +x /mnt/arch-installer-chroot.sh
+# Make the copied chroot script executable within the chroot
+chmod +x /mnt/install_part2_chroot.sh
 
-# Edit sudoers inside /mnt
+# Edit sudoers inside /mnt (already enabled in your previous script)
 echo "Enabling wheel group in sudoers..."
 sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /mnt/etc/sudoers
 
-echo "Entering chroot environment..."
+echo "Entering chroot environment to run Part 2..."
 # Execute the second part of the script within the chroot
-arch-chroot /mnt /arch-installer-chroot.sh --chroot-part "$DEV_PASS" "$ROOT_PASS" "$USER_NAME" "$USER_PASS" "$TIME_ZONE" "$EXISTING_EFI_PARTITION" "$LUKS_PARTITION"
+arch-chroot /mnt /install_part2_chroot.sh "$DEV_PASS" "$ROOT_PASS" "$USER_NAME" "$USER_PASS" "$TIME_ZONE" "$EXISTING_EFI_PARTITION" "$LUKS_PARTITION"
 
 echo "Exiting chroot environment and unmounting..."
 umount -R /mnt
 cryptsetup close lvm
 
-echo "Arch Linux installation (Part 1) complete. Rebooting is recommended."
+echo "Arch Linux installation (Part 1) complete. Please review any errors above and then reboot."
 
 exit 0
-
-# --- Part 2: Chroot System Setup (This part is executed inside the chroot) ---
-if [[ "$1" == "--chroot-part" ]]; then
-    echo "Starting Arch Linux installation script (Part 2: Chroot System Setup)..."
-
-    # Re-assign variables passed from the host script
-    DEV_PASS="$2"
-    ROOT_PASS="$3"
-    USER_NAME="$4"
-    USER_PASS="$5"
-    TIME_ZONE="$6"
-    EXISTING_EFI_PARTITION_IN_CHROOT="$7"
-    LUKS_PARTITION_IN_CHROOT="$8"
-
-    # Install main packages
-    echo "Installing core packages for Wayland, GNOME, and Docker..."
-    # Added 'docker' to the package list
-    pacman -Syu --noconfirm linux linux-headers linux-lts linux-lts-headers base-devel linux-firmware iwd networkmanager nftables net-tools terminator firefox git go keepassxc grub efibootmgr dosfstools os-prober mtools man rsync bash-completion zsh zsh-completions dnsutils gnome reflector tk code amd-ucode nvidia nvidia-lts nvidia-utils xorg-server xorg-apps xorg-xinit xf86-video-amdgpu mesa xorg-xwayland xfsprogs docker
-
-    # Kernel mkinitcpio configuration
-    echo "Updating mkinitcpio configuration..."
-    sed -i "s/HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/g" /etc/mkinitcpio.conf
-    mkinitcpio -P
-
-    # Locale setup
-    echo "Setting up locale..."
-    sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
-    locale-gen
-
-    # User and password setup
-    echo "Setting root password..."
-    echo "root:$ROOT_PASS" | chpasswd
-    echo "Creating user '$USER_NAME' and setting password..."
-    useradd -m -g users -G wheel "$USER_NAME"
-    echo "$USER_NAME:$USER_PASS" | chpasswd
-
-    # Add user to docker group
-    echo "Adding user '$USER_NAME' to the 'docker' group..."
-    gpasswd -a "$USER_NAME" docker
-
-    # Grub installation and configuration
-    echo "Installing Grub..."
-    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck --efi-directory=/boot/EFI
-    cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-    
-    # Configure GRUB for encrypted LVM and Wayland (NVIDIA KMS)
-    echo "Updating GRUB configuration for encrypted LVM and Wayland (NVIDIA KMS if applicable)..."
-    sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=${LUKS_PARTITION_IN_CHROOT}:lvm:allow-discards loglevel=3 quiet nvidia_modeset=1\"/g" /etc/default/grub
-    sed -i "s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=\"1>2\"/g" /etc/default/grub
-    
-    grub-mkconfig -o /boot/grub/grub.cfg
-
-    # Swap setup (Swap File on XFS)
-    echo "Setting up 100GB swap file on XFS filesystem..."
-    fallocate -l 100G /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    # Add to fstab
-    echo '/swapfile none swap defaults 0 0' | tee -a /etc/fstab
-    swapon /swapfile
-
-    # Timezone setup
-    echo "Setting timezone to $TIME_ZONE..."
-    timedatectl set-timezone "$TIME_ZONE"
-
-    # Enable services
-    echo "Enabling systemd services..."
-    systemctl enable NetworkManager
-    systemctl enable systemd-timesyncd
-    systemctl enable gdm
-    systemctl enable docker
-
-    # Remove the installer script from the chroot environment after completion
-    echo "Cleaning up installer script from chroot environment..."
-    rm /arch-installer-chroot.sh
-
-    echo "Arch Linux installation (Part 2) complete. You can now reboot and log in."
-fi
