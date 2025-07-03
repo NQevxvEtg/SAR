1b0ec33833178f5d0966082af4667c137d61efb7
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2025-07-03 16:44:11 +0000

    New commit

--- Code/PowerShell/move-mouse.ps1
+++ Code/PowerShell/move-mouse.ps1
@@ -8,3 +8,16 @@ while ($true)
   [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
   Start-Sleep -Seconds 10
 }
+
+
+
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true)
+{
+  # Randomize within a 500x500 region
+  $x = Get-Random -Minimum 1 -Maximum 500
+  $y = Get-Random -Minimum 1 -Maximum 500
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  Start-Sleep -Seconds 10
+}
